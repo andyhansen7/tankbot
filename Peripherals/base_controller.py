6fe811e8a35aa96f885afe1e3811242f865bbc67
arhansen <arhansen@crimson.ua.edu>
2020-09-19 18:08:39 -0500

    Added primitive turn method to base controller

--- Peripherals/base_controller.py
+++ Peripherals/base_controller.py
@@ -1,15 +1,16 @@
 from base.py import Base
 
-
-# Physical attributes of robot
-CM_PER_STEP = 0.079528
-STEPS_PER_CM = 12.5741877
-BASE_WIDTH_CM = 12.0
-
 class BaseController(self, parent):
     def __init__(self, parent):
         _parent = parent
         _base = Base(self)
+        
+    # Physical attributes of robot
+    CM_PER_STEP = 0.079528
+    STEPS_PER_CM = 12.5741877
+
+    BASE_WIDTH_CM = 14.2
+    PI = 3.141592653589
         
     def DriveCM(self, distance):
         # drive in a straight line
@@ -23,16 +24,18 @@ class BaseController(self, parent):
         
     def TurnInPlaceAngle(self, angle):
         # correct angle to range 0-360, in case it was a negative angle
-        correctedangle = (angle + 360) % 360
+        corrected_angle = (angle + 360) % 360
+        
+        turn_radius = (angle / 360) * (2 * PI * BASE_WIDTH_CM)
         
         # indicates a left hand turn
-        if correctedangle > 180: 
-            base.MoveLeft()
-            base.MoveRight()
+        if corrected_angle > 180: 
+            base.MoveLeft(-1, turn_radius * STEPS_PER_CM, 1)
+            base.MoveRight(1, turn_radius * STEPS_PER_CM, 1)
         # indicates a right hand turn
         else:
-            base.MoveLeft()
-            base.MoveRight()
+            base.MoveLeft(1, turn_radius * STEPS_PER_CM, 1)
+            base.MoveRight(1, turn_radius * STEPS_PER_CM, 1)
             
     def Coast(self):
         base.ReleaseLeft()
